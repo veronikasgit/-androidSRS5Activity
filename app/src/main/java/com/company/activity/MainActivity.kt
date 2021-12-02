1db99deaac3505702eb5b3afafae4e2fac9d2bcd
veronikasgit <w_e_r_o_n_i_k_a@mail.ru>
2021-12-03 02:05:41 +0300

    Clear code

--- app/src/main/java/com/company/activity/MainActivity.kt
+++ app/src/main/java/com/company/activity/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.company.activiti
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 const val TAG = "MainActivity"
 
@@ -11,48 +11,60 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG,"Сижу за решеткой в темнице сырой.\n" +
-                "Вскормленный в неволе орел молодой,")
+        Log.d(
+            TAG, "Сижу за решеткой в темнице сырой.\n" +
+                    "Вскормленный в неволе орел молодой,"
+        )
     }
 
     override fun onStart() {
         super.onStart()
 
-        Log.d(TAG,"Мой грустный товарищ, махая крылом,\n" +
-                "Кровавую пищу клюет под окном,")
+        Log.d(
+            TAG, "Мой грустный товарищ, махая крылом,\n" +
+                    "Кровавую пищу клюет под окном,"
+        )
     }
 
     override fun onResume() {
         super.onResume()
 
-        Log.d(TAG,"Клюет, и бросает, и смотрит в окно,\n" +
-                "Как будто со мною задумал одно;")
+        Log.d(
+            TAG, "Клюет, и бросает, и смотрит в окно,\n" +
+                    "Как будто со мною задумал одно;"
+        )
     }
 
     override fun onPause() {
         super.onPause()
 
-        Log.d(TAG,"Зовет меня взглядом и криком своим\n" +
-                "И вымолвить хочет: «Давай улетим!")
+        Log.d(
+            TAG, "Зовет меня взглядом и криком своим\n" +
+                    "И вымолвить хочет: «Давай улетим!"
+        )
     }
 
     override fun onStop() {
         super.onStop()
 
-        Log.d(TAG,"Мы вольные птицы; пора, брат, пора!\n" +
-                "Туда, где за тучей белеет гора,")
+        Log.d(
+            TAG, "Мы вольные птицы; пора, брат, пора!\n" +
+                    "Туда, где за тучей белеет гора,"
+        )
     }
 
     override fun onRestart() {
         super.onRestart()
 
-        Log.d(TAG,"onRestart")
+        Log.d(TAG, "onRestart")
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        Log.d(TAG,"Туда, где синеют морские края,\n" +
-                "Туда, где гуляем лишь ветер… да я!..»")
+        Log.d(
+            TAG, "Туда, где синеют морские края,\n" +
+                    "Туда, где гуляем лишь ветер… да я!..»"
+        )
     }
 }
